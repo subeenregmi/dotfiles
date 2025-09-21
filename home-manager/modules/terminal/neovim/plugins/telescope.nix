be09645000835bb3f599add6eff2af5a687b1fae
Subeen Regmi <subeenregmi123@gmail.com>
2025-09-21 13:47:45 +0100

    feat: deps + keymaps for telescope

--- home-manager/modules/terminal/neovim/plugins/telescope.nix
+++ home-manager/modules/terminal/neovim/plugins/telescope.nix
@@ -1,3 +1,13 @@
 {
-  programs.nixvim.plugins.telescope.enable = true;
+  programs.nixvim.plugins.telescope = {
+    enable = true;
+    keymaps = {
+      "<leader>ff" = { action = "find_files"; };
+      "<leader>fg" = { action = "live_grep"; };
+      "<leader>fb" = { action = "buffers"; };
+      "<leader>fh" = { action = "help_tags"; };
+      "<leader>fo" = { action = "old_files"; };
+      "<leader>fd" = { action = "diagnostics"; };
+    };
+  };
 }
