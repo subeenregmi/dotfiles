fb2f086fb5eb6faffc2a59c858390e9c66b7a8c2
Subeen Regmi <subeenregmi123@gmail.com>
2025-11-16 13:05:50 +0000

    f

--- home-manager/modules/terminal/neovim/autocmds.nix
+++ home-manager/modules/terminal/neovim/autocmds.nix
@@ -14,20 +14,19 @@
   }
   {
     event = [ "RecordingEnter" "RecordingLeave"];
+    desc = "Notify when recording a macro";
+    group = "macro-notify";
     callback = {
       __raw = ''
-        desc = 'Notify when recording a macro',
-        group = augroup 'macro-notify',
-        callback = function(ev)
-            local msg
+        function(ev)
+          local msg
           if ev.event == 'RecordingEnter' then
             msg = 'Recording to register @'
           else
             msg = 'Recorded to register @'
           end
-          vim.notify(msg .. vim.fn.reg_recording(), vim.log.levels.INFO, { title = 'Macro', timeout = 5000, hide_from_hist          ↪ory = false })
-        end,
-
+          vim.notify(msg .. vim.fn.reg_recording(), vim.log.levels.INFO, { title = 'Macro', timeout = 5000, hide_from_history = false })
+        end
       '';
     };
   }
--- home-manager/modules/terminal/neovim/plugins/noice.nix
+++ home-manager/modules/terminal/neovim/plugins/noice.nix
@@ -1,3 +1,4 @@
+{ 
   programs.nixvim.plugins.noice = {
     enable = true;
     settings = {
